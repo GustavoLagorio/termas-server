65e449c531b20f7175f7891316c76b16c7943a08
Gustavo Lagorio <lagoriog3287@gmail.com>
2023-10-09 20:17:05 -0300

    retiramos el payload para el JWT cliente

--- helpers/jwtCliente.js
+++ helpers/jwtCliente.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const generarTokenCliente = (payload) => {
-    return jwt.sign(payload, process.env.SECRET_JWT_SEED, { expiresIn: '1h' });
+const generarTokenCliente = () => {
+    return jwt.sign({}, process.env.SECRET_JWT_SEED, { expiresIn: '1h' });
 };
 
 module.exports = {
