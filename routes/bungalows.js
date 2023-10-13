15df133169bcdc9dcaf6a3e71b75d71cb78f966c
Gustavo Lagorio <lagoriog3287@gmail.com>
2023-10-13 19:29:32 -0300

    se agrega check de ubicacion

--- routes/bungalows.js
+++ routes/bungalows.js
@@ -16,9 +16,9 @@ router.post(
     [ //middlewares
         check('idBungalow', 'El Id es obligatorio').not().isEmpty(),
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-        check('descripcion', 'La descripcion es obligatoria').not().isEmpty(),
-        check('imagenes', 'Las imagenes son obligatorias').not().isEmpty(),
+        check('descripcion', 'La descripcion es obligatoria').not().isEmpty(),        
         check('galeria', 'La galeria es obligatoria').not().isEmpty(),
+        check('ubicacion', 'La ubicacion es obligatoria').not().isEmpty(),
         check('comodidades', 'Las comodidades son obligatorias').not().isEmpty(),
         check('precio', 'El precio es obligatorio').not().isEmpty(),
         validarJWT,
